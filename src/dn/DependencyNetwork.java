4b5ac43174e18ce1341c01bf5afc5c683a570f3b
henryzord <henry.cagnini@gmail.com>
2020-01-24 14:25:35 -0300

    implementing dependencyNetwork

--- src/dn/DependencyNetwork.java
+++ src/dn/DependencyNetwork.java
@@ -1,40 +1,77 @@
 package dn;
 
+import javafx.scene.transform.MatrixType;
+import org.apache.commons.math3.random.MersenneTwister;
+
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
+import java.lang.reflect.Array;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Hashtable;
 import java.util.stream.Stream;
 
 public class DependencyNetwork {
-    public DependencyNetwork(String variables_path) throws Exception {
+    private Hashtable<String, Variable> variables;
+
+    public DependencyNetwork(MersenneTwister mt, String variables_path) throws Exception {
         Object[] algorithms = Files.list(new File(variables_path).toPath()).toArray();
+
+        variables = new Hashtable<>();
+
         for(int i = 0; i < algorithms.length; i++) {
-            Object[] variables = Files.list(new File(((Path)algorithms[i]).toString()).toPath()).toArray();
-            for(int j = 0; j < variables.length; j++) {
-                BufferedReader csvReader = new BufferedReader(new FileReader(((Path)variables[j]).toString()));
-                String row;
-                int n_columns = -1;
-                ArrayList<String> data = new ArrayList<>();
+            Object[] variables_names  = Files.list(new File(algorithms[i].toString()).toPath()).toArray();
+            for(int j = 0; j < variables_names.length; j++) {
+                BufferedReader csvReader = new BufferedReader(new FileReader(variables_names[j].toString()));
+
+                String row = csvReader.readLine();
+
+                String[] this_data = row.split(",");
+                String[] header = this_data;
+
+                String variableName = this_data[this_data.length - 2];
+                int n_variables_table = 1;
+                String[] parents = null;
+
+                Hashtable<String, Hashtable<String, ArrayList<Integer>>> table = new Hashtable<>(n_variables_table);
+                table.put(variableName, new Hashtable<String, ArrayList<Integer>>());
+
+                if(this_data.length > 2) {
+                    parents = new String [this_data.length - 2];
+                    n_variables_table = n_variables_table + parents.length;
+                    for(int k = 0; k < this_data.length - 2; k++) {
+                        parents[k] = this_data[k];
+                        table.put(parents[k], new Hashtable<String, ArrayList<Integer>>());
+                    }
+                }
+
+                ArrayList<Float> probabilities = new ArrayList<>((int)Math.pow(n_variables_table, 2));
+
+                int index = 0;
                 while ((row = csvReader.readLine()) != null) {
-                    String[] this_data = row.split(",");
-                    n_columns = this_data.length;
-                    data.addAll(Arrays.asList(this_data));
+                    this_data = row.split(",");
+                    probabilities.add(Float.valueOf(this_data[this_data.length - 1]));
+                    for(int k = 0; k < this_data.length - 1; k++) {
+                        table.get(header[k]).put(this_data[k], new ArrayList<>());
+                        table.get(header[k]).get(this_data[k]).add(index);
+                    }
+                    index++;
                 }
                 csvReader.close();
-                ConditionalTable ct = new ConditionalTable(data, n_columns);
+//                variables.put(variableName, new Variable(variableName, parents, table, mt));
             }
-
             int z = 0;
         }
     }
 
     public static void main(String[] args) throws Exception {
-        String variables_path = "/home/henry/Projects/WekaCustom/distributions";
+        String variables_path = "/home/henry/Projects/ednel/resources/distributions";
 
-        DependencyNetwork dn = new DependencyNetwork(variables_path);
+        MersenneTwister mt = new MersenneTwister();
+
+        DependencyNetwork dn = new DependencyNetwork(mt, variables_path);
     }
 }
--- src/dn/Variable.java
+++ src/dn/Variable.java
@@ -1,133 +1,61 @@
 package dn;
 
-public class Variable {
-    private String[] values;
-    private float[] probabilities;
+import org.apache.commons.math3.random.MersenneTwister;
 
-    public Variable(String[] values, float[] probabilities) {
-        this.values = values;
-        this.probabilities = probabilities;
+import java.util.ArrayList;
+import java.util.Hashtable;
+
+public class Variable {
+    private String name;
+    private String[] parents;
+    private ArrayList<ArrayList<String>> table;
+    private MersenneTwister number_generator;
+
+    public Variable(String name, String[] parents, ArrayList<ArrayList<String>> table, MersenneTwister number_generator) {
+        this.name = name;
+        this.parents = parents;
+        this.table = table;
+        this.number_generator = number_generator;
     }
 
-    public String sample() {
-//        static char *kwds[] = {"a", "size", "replace", "p", NULL};
-//    PyObject *size = NULL, *a = NULL, *p = NULL;
-//    int replace = 1;
-//
-//    if (!PyArg_ParseTupleAndKeywords(args, kwargs, "O|OpO", kwds, &a, &size, &replace, &p)) {
-//        return NULL;
-//    }
-//
-//    if(!PyArray_Check(a)) {
-//        PyErr_SetString(PyExc_TypeError, "a must be a numpy.ndarray");
-//        return NULL;
-//    }
-//    if((p != NULL) && !PyArray_Check(p)) {
-//        PyErr_SetString(PyExc_TypeError, "p must be a numpy.ndarray");
-//        return NULL;
-//    }
-//
-//    int sampled_ndims = -1;
-//    npy_intp *sampled_dims;
-//    if(size != NULL) {
-//        if(PyTuple_Check(size)) {
-//            sampled_ndims = (int)PyTuple_Size(size);
-//            sampled_dims = new npy_intp [sampled_ndims];
-//
-//            for(int i = 0; i < sampled_ndims; i++) {
-//                sampled_dims[i] = (int)PyLong_AsLong(PyTuple_GetItem(size, i));
-//            }
-//        } else if (PyLong_Check(size)) {
-//            sampled_ndims = 1;
-//            sampled_dims = new npy_intp [1];
-//            sampled_dims[0] = (int)PyLong_AsLong(size);
-//        } else {
-//            PyErr_SetString(PyExc_TypeError, "size must be either a tuple or an integer");
-//            return NULL;
-//        }
-//    } else {
-//        PyErr_SetString(PyExc_NotImplementedError, "not implemented yet!");
-//        return NULL;
-//    }
-//
-//    int a_ndims = PyArray_NDIM((const PyArrayObject*)a),
-//        p_ndims = PyArray_NDIM((const PyArrayObject*)p);
-//
-//    if(a_ndims > 1) {
-//        PyErr_SetString(PyExc_ValueError, "a must be 1-dimensional");
-//        return NULL;
-//    }
-//    if(p_ndims > 1) {
-//        PyErr_SetString(PyExc_ValueError, "p must be 1-dimensional");
-//        return NULL;
-//    }
-//
-//    npy_intp *a_dims = PyArray_SHAPE((PyArrayObject*)a),
-//             *p_dims = PyArray_SHAPE((PyArrayObject*)p);
-//
-//    for(int j = 0; j < a_ndims; j++) {
-//        if(a_dims[j] != p_dims[j]) {
-//            PyErr_SetString(PyExc_ValueError, "a and p must have the same size");
-//            return NULL;
-//        }
-//    }
-//
-//    PyArray_Descr *a_descr = PyArray_DESCR((PyArrayObject*)a);
-//    PyObject *sampled_obj = PyArray_NewFromDescr(
-//        &PyArray_Type, a_descr,
-//        sampled_ndims, sampled_dims, NULL, NULL, NPY_ARRAY_C_CONTIGUOUS | NPY_ARRAY_WRITEABLE, NULL
-//    );
-//
-//    PyArrayObject *sampled = (PyArrayObject*)sampled_obj;
-//
-//    if(sampled == NULL) {
-//        PyErr_SetString(PyExc_ValueError, "Exception ocurred while trying to allocate space for numpy array");
-//        return NULL;
-//    }
-//
-//    // type checking ends here; real code begins here
-//
-//    npy_intp sampled_size = PyArray_SIZE(sampled),
-//             a_itemsize = PyArray_ITEMSIZE((PyArrayObject*)a),
-//             p_itemsize = PyArray_ITEMSIZE((PyArrayObject*)p);
-//
-//    int *sampled_counters = (int*)malloc(sizeof(int) * sampled_ndims);
-//    for(int j = 0; j < sampled_ndims; j++) {
-//        sampled_counters[j] = 0;
-//    }
-//
-//    char *sampled_ptr = PyArray_BYTES(sampled), *p_ptr, *a_ptr;  // data pointers
-//    npy_intp sampled_itemsize = PyArray_ITEMSIZE(sampled);
-//
-//    int num, div;
-//    float sum, spread = 1000, p_data;
-//    PyObject *a_data;
-//
-//    for(int i = 0; i < sampled_size; i++) {
-//        num = rand() % (int)spread;  // random sampled number
-//        sum = 0;  // sum of probabilities so far
-//
-//        p_ptr = PyArray_BYTES((PyArrayObject*)p);
-//        a_ptr = PyArray_BYTES((PyArrayObject*)a);
-//
-//        for(int k = 0; k < a_dims[0]; k++) {
-//            p_data = (float)PyFloat_AsDouble(PyArray_GETITEM((PyArrayObject*)p, p_ptr));
-//            a_data = PyArray_GETITEM((PyArrayObject*)a, a_ptr);
-//            p_ptr += p_itemsize;
-//            a_ptr += a_itemsize;
-//
-//            div = (int)(num/((sum + p_data) * spread));
-//
-//            if(div <= 0) {
-//                PyArray_SETITEM(sampled, sampled_ptr, a_data);
-//                break;
-//            }
-//            sum += p_data;
-//        }
-//        sampled_ptr += sampled_itemsize;
-//    }
-//
-//    return Py_BuildValue("O", sampled);
+    public String[] unconditionalSampling(int sample_size) {
         return null;
     }
+
+    public String[] conditionalSampling(Hashtable<String, String> evidence, int sample_size) {
+
+//        float sum, num, spread = 1000;  // spread is used to guarantee that numbers up to third decimal will be sampled
+//
+//        String[] sampled = new String [sample_size];
+//
+//        for(int i = 0; i < sample_size; i++) {
+//            num = number_generator.nextInt((int)spread) / spread;
+//            sum = 0;
+//
+//            this.table.get(evidence)
+//
+//            for(int k = 0; k < values.length; k++) {
+//                if((sum < num) && (num <= (sum + probabilities[k]))) {
+//                    sampled[i] = values[k];
+//                    break;
+//                } else {
+//                    sum += probabilities[k];
+//                }
+//            }
+//        }
+//        return sampled;
+        return null;
+    }
+
+    public static void main(String[] args) {
+//        MersenneTwister mt = new MersenneTwister();
+//
+//        Variable v = new Variable(mt, new String[]{"a", "b", "c"}, new float[]{(float)0.5, (float)0.4, (float)0.1});
+//        String[] samples = v.conditionalSampling(100);
+//        for(int i = 0; i < samples.length; i++) {
+//            System.out.print(samples[i]);
+//        }
+
+    }
+
 }
